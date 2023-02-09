c8d3489bea6fd82aeaaeca461e41958ec07d8c6b
Marco Tettamanti <marcotettamanti@outlook.com.br>
2023-02-09 19:44:49 -0300

    H2 database

--- src/main/java/com/portfolio/course/entities/User.java
+++ src/main/java/com/portfolio/course/entities/User.java
@@ -2,10 +2,19 @@ package com.portfolio.course.entities;
 
 import java.io.Serializable;
 
-public class User implements Serializable{
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "tb_user")//rename object name for database
+public class User implements Serializable{
   private static final long serialVersionUID = 1L;
   
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)//autoincremented
   private Long id;
   private String name;
   private String email;
