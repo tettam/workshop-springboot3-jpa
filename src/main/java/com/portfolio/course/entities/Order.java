575f64be4d16903210d228a349da968db2b2dd4b
Marco Tettamanti <marcotettamanti@outlook.com.br>
2023-02-12 20:03:14 -0300

    OrderStatus Enum

--- src/main/java/com/portfolio/course/entities/Order.java
+++ src/main/java/com/portfolio/course/entities/Order.java
@@ -4,9 +4,12 @@ import java.io.Serializable;
 
 import java.time.Instant;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
+import com.portfolio.course.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GenerationType;
@@ -28,32 +31,51 @@ public class Order implements Serializable{
   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
   private Instant moment;
 
+  private Integer orderStatus;
+
   @JsonIgnore
   @ManyToOne //entidade pode ter muitos relacionamentos com outra entidad
   @JoinColumn(name = "client_id") //relacionamento entre as entidades é armazenado em uma coluna na tabela "Order".
   private User client;
 
   public Order() {}
-  public Order(Long id, Instant moment, User client) {
+  public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
     this.id = id;
     this.moment = moment;
+    setOrderStatus(orderStatus);
     this.client = client;
   }
+
   public Long getId() {
     return id;
   }
+
   public void setId(Long id) {
     this.id = id;
   }
+
   public Instant getMoment() {
     return moment;
   }
+
   public void setMoment(Instant moment) {
     this.moment = moment;
   }
+
+  public OrderStatus getOrderStatus() {
+    return OrderStatus.valueOf(orderStatus);
+  }
+
+  public void setOrderStatus(OrderStatus orderStatus) {
+    if(orderStatus != null) {
+      this.orderStatus = orderStatus.getCode();
+    }
+  }
+
   public User getClient() {
     return client;
   }
+
   public void setClient(User client) {
     this.client = client;
   }
