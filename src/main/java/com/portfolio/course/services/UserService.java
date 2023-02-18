6f756a6743b434d17f5010894d1d4d71a109a848
Marco Tettamanti <marcotettamanti@outlook.com.br>
2023-02-18 00:40:42 -0300

    User update

--- src/main/java/com/portfolio/course/services/UserService.java
+++ src/main/java/com/portfolio/course/services/UserService.java
@@ -33,5 +33,18 @@ public class UserService {
     repository.deleteById(id);
   }
 
+  public User update(Long id, User obj){
+    User entity = repository.getReferenceById(id); // Objeto fica monitorado antes de ir para o banco de dados
+    updateData(entity, obj);
+    return repository.save(entity);
+  }
+
+  private void updateData(User entity, User obj) {
+    entity.setName(obj.getName());
+    entity.setEmail(obj.getEmail());
+    entity.setPhone(obj.getPhone());
+    
+  }
+
 
 }
