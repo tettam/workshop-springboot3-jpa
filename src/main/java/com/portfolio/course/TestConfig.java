a5609aaef02697cc1f1fe7bc645f039e0280b725
Marco Tettamanti <marcotettamanti@outlook.com.br>
2023-02-17 23:49:33 -0300

    Payment, one-to-one

--- src/main/java/com/portfolio/course/TestConfig.java
+++ src/main/java/com/portfolio/course/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.portfolio.course.entities.Category;
 import com.portfolio.course.entities.Order;
 import com.portfolio.course.entities.OrderItem;
+import com.portfolio.course.entities.Payment;
 import com.portfolio.course.entities.Product;
 import com.portfolio.course.entities.User;
 import com.portfolio.course.entities.enums.OrderStatus;
@@ -80,5 +81,10 @@ public class TestConfig implements CommandLineRunner{//ComandLineRunner(Executa 
     OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
     
     orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+    Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+    o1.setPayment(pay1);
+    //A
+    orderRepository.save(o1);
   }
 }
--- src/main/java/com/portfolio/course/entities/Order.java
+++ src/main/java/com/portfolio/course/entities/Order.java
@@ -10,6 +10,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.portfolio.course.entities.enums.OrderStatus;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.GeneratedValue;
@@ -17,6 +18,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -40,6 +42,9 @@ public class Order implements Serializable{
 
   @OneToMany(mappedBy = "id.order") // uma ordem para vários pedidos
   private Set<OrderItem> items = new HashSet<>(); //retorna uma lista de OrderItem
+
+  @OneToOne(mappedBy = "order", cascade = CascadeType.ALL) //Mesmo id
+  private Payment payment;
 
   public Order() {}
   public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
@@ -85,6 +90,14 @@ public class Order implements Serializable{
 
   public Set<OrderItem> getItems() {
     return items;
+  }
+  
+  public Payment getPayment() {
+    return payment;
+  }
+
+  public void setPayment(Payment payment) {
+    this.payment = payment;
   }
 
   @Override
